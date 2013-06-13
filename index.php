3ba6e344cd55b6e49cf0ca37c929ee477e773885
Kevin Kirchner <kevin@classyllama.com>
2013-06-13 09:19:57 -0500

    initial files

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="http://cdn.jsdelivr.net/chosen/0.9.12/chosen.css" type="text/css" media="screen" title="chosen" charset="utf-8">
+        <link rel="stylesheet" href="/css/main.css" type="text/css" media="all" title="" charset="utf-8">
+    </head>
+    <body>
+        <!-- Passage Loader -->
+        <header>
+        </header>
+
+        <!-- Passage Content -->
+        <section>
+        </section>
+
+        <!-- Peripherals -->
+        <aside>
+        </aside>
+        
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.10.0.min.js" charset="utf-8"></script>
+        <script type="text/javascript" src="http://cdn.jsdelivr.net/chosen/0.9.12/chosen.jquery.min.js" charset="utf-8"></script>
+        <script type="text/javascript" src="/js/main.js" charset="utf-8"></script>
+    </body>
+</html>
