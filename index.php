351a1c0c81dfe1682190a9fa19ae592025298585
Kevin Kirchner <kevin@classyllama.com>
2013-06-14 09:54:02 -0500

    # some markup and adding font-awesome

--- index.php
+++ index.php
@@ -2,16 +2,76 @@
 <html>
     <head>
         <title></title>
-        <link rel="stylesheet" href="http://cdn.jsdelivr.net/chosen/0.9.12/chosen.css" type="text/css" media="screen" title="chosen" charset="utf-8">
-        <link rel="stylesheet" href="/css/main.css" type="text/css" media="all" title="" charset="utf-8">
+        <!-- <link rel="stylesheet" href="http://cdn.jsdelivr.net/chosen/0.9.12/chosen.css" type="text/css" media="screen" title="chosen" charset="utf-8"> -->
+        <link rel="stylesheet" href="/assets/vendor/chosen/chosen.css" type="text/css" media="all" title="chosen" charset="utf-8">
+        
+        <!-- <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet"> -->
+        <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.0/css/font-awesome.css" rel="stylesheet">
+        <!-- <link rel="stylesheet" href="/assets/vendor/font-awesome/css/font-awesome.min.css" type="text/css" media="all" title="font-awesome" charset="utf-8"> -->
+        <!--[if lte IE 7]> <link rel="stylesheet" href="/assets/vendor/font-awesome/css/font-awesome-ie7.min.css" type="text/css" media="all" title="font-awesome-ie7" charset="utf-8"> <![endif]-->
+        
+        <link rel="stylesheet" href="/assets/css/main.css" type="text/css" media="all" title="main" charset="utf-8">
     </head>
     <body>
         <!-- Passage Loader -->
         <header>
+            <ul id="nav">
+                <li class="top">
+                    <a href="#" class="icon-book"> Read</a>
+                    <ul class="sub">
+                        <li><i class="icon-spinner icon-spin"></i> Loading Plans</li>
+                        <!-- If nothing loaded - connect/re-connect -->
+                        <li><a href="#">Plan 1 ([last read text])</a></li>
+                        <li><a href="#">Plan 2 ([last read text])</a></li>
+                        <li class="add">
+                            <a href="#"><i class="icon-plus"></i> Add A Reading Plan</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="#" class="icon-bookmark"> Reference 1</a></li>
+                        <li class="add">
+                            <a href="#">
+                                <span class="icon-stack">
+                                    <i class="icon-bookmark-empty icon-stack-base"></i>
+                                    <i class="icon-plus"></i>
+                                </span>
+                                Add A Bookmark
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="top">
+                    <a href="#" class="icon-search"> Search</a>
+                    <div class="sub">
+                        
+                    </div>
+                </li>
+                <li class="top">
+                    <a href="#" class="icon-book"> Menu</a>
+                    <ul class="sub">
+                        <li>
+                            <a href="#" class="icon-circle"> Light</a>
+                            <a href="#" class="icon-circle-blank"> Dark</a>
+                        </li>
+                        <li>
+                            <a href="#" class="icon-font"> Font Style</a>
+                        </li>
+                        <li></li>
+                    </ul>
+                </li>
+                <li class="top">
+                    <a href="#" class="icon-share"> Share</a>
+                    <ul class="sub">
+                        <li><a href="#" class="icon-facebook"> Facebook</a></li>
+                        <li><a href="#" class="icon-twitter"> Twitter</a></li>
+                        <li><a href="#" class="icon-envelope-alt"> Email</a></li>
+                    </ul>
+                </li>
+            </ul>
         </header>
 
         <!-- Passage Content -->
         <section>
+            <h2>Matthew 22</h2><h4><i>The Parable of the Wedding Banquet</i></h4><b>1</b> <p class="bodytext">Jesus spoke to them again in parables, saying: <b>2</b> “The kingdom of heaven can be compared to a king who gave a wedding banquet for his son. <b>3</b> He sent his slaves to summon those who had been invited to the banquet, but they would not come. <b>4</b> Again he sent other slaves, saying, ‘Tell those who have been invited, “Look! The feast I have prepared for you is ready. My oxen and fattened cattle have been slaughtered, and everything is ready. Come to the wedding banquet.”’ <b>5</b> But they were indifferent and went away, one to his farm, another to his business. <b>6</b> The rest seized his slaves, insolently mistreated them, and killed them. <b>7</b> The king was furious! He sent his soldiers, and they put those murderers to death and set their city on fire. <b>8</b> Then he said to his slaves, ‘The wedding is ready, but the ones who had been invited were not worthy. <b>9</b> So go into the main streets and invite everyone you find to the wedding banquet.’ <b>10</b> And those slaves went out into the streets and gathered all they found, both bad and good, and the wedding hall was filled with guests. <b>11</b> But when the king came in to see the wedding guests, he saw a man there who was not wearing wedding clothes. <b>12</b> And he said to him, ‘Friend, how did you get in here without wedding clothes?’ But he had nothing to say. <b>13</b> Then the king said to his attendants, ‘Tie him up hand and foot and throw him into the outer darkness, where there will be weeping and gnashing of teeth!’ <b>14</b> For many are called, but few are chosen.”</p> <div class="spacer"></div><h4><i>Paying Taxes to Caesar</i></h4><b>15</b> <p class="bodytext">Then the Pharisees went out and planned together to entrap him with his own words. <b>16</b> They sent to him their disciples along with the Herodians, saying, “Teacher, we know that you are truthful, and teach the way of God in accordance with the truth. You do not court anyone’s favor because you show no partiality. <b>17</b> Tell us then, what do you think? Is it right to pay taxes to Caesar or not?”</p> <b>18</b> <p class="bodytext">But Jesus realized their evil intentions and said, “Hypocrites! Why are you testing me? <b>19</b> Show me the coin used for the tax.” So they brought him a denarius. <b>20</b> Jesus said to them, “Whose image is this, and whose inscription?” <b>21</b> They replied, “Caesar’s.” He said to them, “Then give to Caesar the things that are Caesar’s, and to God the things that are God’s.” <b>22</b> Now when they heard this they were stunned, and they left him and went away.</p> <div class="spacer"></div><h4><i>Marriage and the Resurrection</i></h4><b>23</b> <p class="bodytext">The same day Sadducees (who say there is no resurrection) came to him and asked him, <b>24</b> “Teacher, Moses said, ‘<b>If a man dies without having children, his brother must marry the widow and father children</b> <b>for his brother</b>.’ <b>25</b> Now there were seven brothers among us. The first one married and died, and since he had no children he left his wife to his brother. <b>26</b> The second did the same, and the third, down to the seventh. <b>27</b> Last of all, the woman died. <b>28</b> In the resurrection, therefore, whose wife of the seven will she be? For they all had married her.” <b>29</b> Jesus answered them, “You are deceived, because you don’t know the scriptures or the power of God. <b>30</b> For in the resurrection they neither marry nor are given in marriage, but are like angels in heaven. <b>31</b> Now as for the resurrection of the dead, have you not read what was spoken to you by God, <b>32</b> ‘<b>I am the God of Abraham, the God of Isaac, and the God of Jacob</b>’? He is not the God of the dead but of the living!” <b>33</b> When the crowds heard this, they were amazed at his teaching.</p> <div class="spacer"></div><h4><i>The Greatest Commandment</i></h4><b>34</b> <p class="bodytext">Now when the Pharisees heard that he had silenced the Sadducees, they assembled together. <b>35</b> And one of them, an expert in religious law, asked him a question to test him: <b>36</b> “Teacher, which commandment in the law is the greatest?” <b>37</b> Jesus said to him, “‘<b>Love</b> <b>the Lord your God with all your heart, with all your soul, and with all your mind</b>.’ <b>38</b> This is the first and greatest commandment. <b>39</b> The second is like it: ‘<b>Love your neighbor as yourself</b>.’ <b>40</b> All the law and the prophets depend on these two commandments.”</p> <div class="spacer"></div><h4><i>The Messiah: David’s Son and Lord</i></h4><b>41</b> <p class="bodytext">While the Pharisees were assembled, Jesus asked them a question: <b>42</b> “What do you think about the Christ? Whose son is he?” They said, “The son of David.” <b>43</b> He said to them, “How then does David by the Spirit call him ‘Lord,’ saying,</p> <b>44</b> <p class="otpoetry">‘<b>The Lord said to my lord,</b></p> <p class="otpoetry">“<b>Sit at my right hand,</b></p> <p class="otpoetry"><b>until I put your enemies under your feet</b>”’?</p> <b>45</b> <p class="bodyblock">If David then calls him ‘Lord,’ how can he be his son?” <b>46</b> No one was able to answer him a word, and from that day on no one dared to question him any longer.</p> <a style="" target="_blank" href="http://bible.org/page.php?page_id=3537">©NET</a> 
         </section>
 
         <!-- Peripherals -->
@@ -20,6 +80,6 @@
         
         <script type="text/javascript" src="http://code.jquery.com/jquery-1.10.0.min.js" charset="utf-8"></script>
         <script type="text/javascript" src="http://cdn.jsdelivr.net/chosen/0.9.12/chosen.jquery.min.js" charset="utf-8"></script>
-        <script type="text/javascript" src="/js/main.js" charset="utf-8"></script>
+        <script type="text/javascript" src="/assets/js/main.js" charset="utf-8"></script>
     </body>
 </html>
