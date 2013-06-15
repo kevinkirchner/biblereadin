62dda7fc6edab1931828023278400a10decc4ca2
Kevin Kirchner <kevin@classyllama.com>
2013-06-14 23:18:15 -0500

    a LOT of styling done, plus responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 
+(function($, window, undefined){
+    $('#nav .top').on('mouseenter', function(){
+        $(this).addClass('hover');
+    }).on('mouseleave', function(){
+        $(this).removeClass('hover');
+    });
+    
+    
+})(jQuery, this);
