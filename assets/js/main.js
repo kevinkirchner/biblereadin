8982d351f19a46d51d316e367b2cb66659401c20
Kevin Kirchner <kirchner.kevin@gmail.com>
2013-06-23 00:50:15 -0500

    doing a good amount of work on localstorage and the intro tour

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 var CURRENT_PASSAGE;
-var IS_PUBLIC_COMPUTER = amplify.store('br_is_public_computer') || false;
+
 var bible = {
     allBooks: ["Genesis","Exodus","Leviticus","Numbers","Deuteronomy","Joshua","Judges","Ruth","1 Samuel","2 Samuel","1 Kings","2 Kings","1 Chronicles","2 Chronicles","Ezra","Nehemiah","Ester","Job","Psalms","Proverbs","Ecclesiastes","Song of Solomon","Isaiah","Jeremiah","Lamentations","Ezekiel","Daniel","Hosea","Joel","Amos","Obadiah","Jonah","Micah","Nahum","Habakkuk","Zephaniah","Haggai","Zechariah","Malachi","Matthew","Mark","Luke","John","Acts","Romans","1 Corinthians","2 Corinthians","Galatians","Ephesians","Philippians","Colossians","1 Thessalonians","2 Thessalonians","1 Timothy","2 Timothy","Titus","Philemon","Hebrews","James","1 Peter","2 Peter","1 John","2 John","3 John","Jude","Revelation"],
     otBooks: ["Genesis","Exodus","Leviticus","Numbers","Deuteronomy","Joshua","Judges","Ruth","1 Samuel","2 Samuel","1 Kings","2 Kings","1 Chronicles","2 Chronicles","Ezra","Nehemiah","Ester","Job","Psalms","Proverbs","Ecclesiastes","Song of Solomon","Isaiah","Jeremiah","Lamentations","Ezekiel","Daniel","Hosea","Joel","Amos","Obadiah","Jonah","Micah","Nahum","Habakkuk","Zephaniah","Haggai","Zechariah","Malachi"],
@@ -16,6 +16,12 @@ var bible = {
     ntProphecy: ["Revelation"],
     chapterCount: {"Genesis":50,"Exodus":40,"Leviticus":27,"Numbers":36,"Deuteronomy":34,"Joshua":24,"Judges":21,"Ruth":4,"1 Samuel":31,"2 Samuel":24,"1 Kings":22,"2 Kings":25,"1 Chronicles":29,"2 Chronicles":36,"Ezra":10,"Nehemiah":13,"Esther":10,"Job":42,"Psalms":150,"Proverbs":31,"Ecclesiastes":12,"Song of Songs":8,"Isaiah":66,"Jeremiah":52,"Lamentations":5,"Ezekiel":48,"Daniel":12,"Hosea":14,"Joel":3,"Amos":9,"Obadiah":1,"Jonah":4,"Micah":7,"Nahum":3,"Habakkuk":3,"Zephaniah":3,"Haggai":2,"Zechariah":14,"Malachi":4,"Matthew":28,"Mark":16,"Luke":24,"John":21,"Acts":28,"Romans":16,"1 Corinthians":16,"2 Corinthians":13,"Galatians":6,"Ephesians":6,"Philippians":4,"Colossians":4,"1 Thessalonians":5,"2 Thessalonians":3,"1 Timothy":6,"2 Timothy":4,"Titus":3,"Philemon":1,"Hebrews":13,"James":5,"1 Peter":5,"2 Peter":3,"1 John":5,"2 John":1,"3 John":1,"Jude":1,"Revelation": 22}
 };
+
+function clearStorage(){
+    for (v in amplify.store()) {
+        amplify.store(v,null);
+    }
+}
 
 function setCurrentPassage(psg) {
     CURRENT_PASSAGE = psg;
@@ -123,6 +129,7 @@ function getURLParameter(name) {
     // Onload
     loadPassage( getURLParameter('psg') || getRandomPassage() );
     
+    var BR = amplify.store() || false;
     
     // Navigation ------------------------------------------//
     var $search = $('#search');
@@ -170,12 +177,12 @@ function getURLParameter(name) {
         var themeColor = el.data('theme-color');
         $('body').removeClass(lastTheme).addClass( themeColor );
         lastTheme = themeColor;
-        amplify.store('br_theme',themeColor);
+        amplify.store('theme',themeColor);
         $(this).parents('.top').removeClass('hover');
         return false;
     });
     
-    var savedTheme = amplify.store('br_theme') || 'light';
+    var savedTheme = BR.theme || 'light';
     if (savedTheme != 'light') {
         setTimeout(function(){
             $('.theme-link[data-theme-color="'+savedTheme+'"]').trigger('click');
@@ -189,12 +196,12 @@ function getURLParameter(name) {
         var font = el.data('font');
         $('body').removeClass(lastFont).addClass( font );
         lastFont = font;
-        amplify.store('br_font',font);
+        amplify.store('font',font);
         $(this).parents('.top').removeClass('hover');
         return false;
     });
     
-    var savedFont = amplify.store('br_font') || 'sans';
+    var savedFont = BR.font || 'sans';
     if (savedFont != 'sans') {
         $('.font-link[data-font="'+savedFont+'"]').trigger('click');
     }
@@ -239,5 +246,11 @@ function getURLParameter(name) {
         loadPassage( el.data('psg') );
         return false;
     })
+    
+    // Aplify Stuff
+    if (typeof BR != undefined) {
+        // Show modal
+        $('#intro').modal('show')
+    }
     
 })(jQuery, this);
